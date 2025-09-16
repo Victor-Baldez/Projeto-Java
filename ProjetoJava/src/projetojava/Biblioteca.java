e84e322c68c2443cab3110098ace64060224840d
higor240 <higorferreira240@gmail.com>
2025-09-15 21:52:45 -0300

    criacao das variaveis da classe biblioteca

---
+++ ProjetoJava/src/projetojava/Biblioteca.java
@@ -0,0 +1,9 @@
+package projetojava;
+
+import java.util.List;
+
+public class Biblioteca {
+    private int usuarioId;
+    private List Jogosadquiridos; /* vai ser arrayList no futuro. */
+    
+}
