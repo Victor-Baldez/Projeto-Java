c8c42b3cd1df2465b5eda80ee5b2dcbf620751aa
higor240 <higorferreira240@gmail.com>
2025-09-15 21:54:05 -0300

    criacao dos metodos get e set

--- ProjetoJava/src/projetojava/Biblioteca.java
+++ ProjetoJava/src/projetojava/Biblioteca.java
@@ -5,5 +5,31 @@ import java.util.List;
 public class Biblioteca {
     private int usuarioId;
     private List Jogosadquiridos; /* vai ser arrayList no futuro. */
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public List getJogosadquiridos() {
+        return Jogosadquiridos;
+    }
+
+    public void setJogosadquiridos(List Jogosadquiridos) {
+        this.Jogosadquiridos = Jogosadquiridos;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
