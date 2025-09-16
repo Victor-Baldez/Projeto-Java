8b00167bd7c17da0236e0338477550836dd11250
higor240 <higorferreira240@gmail.com>
2025-09-15 22:09:29 -0300

    criacao das variaveis da classe jogo

---
+++ ProjetoJava/src/projetojava/Jogo.java
@@ -0,0 +1,18 @@
+package projetojava;
+
+
+public class Jogo {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private double preco;
+    private String dataLancamento;
+    private String desenvolvedora;
+    private String categoria;
+    private double tamanhoArquivo;
+    
+    
+    
+    
+    
+}
