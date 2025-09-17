d8e6121d9e6b0822216e231738bb329c99ebddad
higor240 <higorferreira240@gmail.com>
2025-09-16 21:39:20 -0300

    Criacao da classe categoria e menu

--- ProjetoJava/src/projetojava/Jogo.java
+++ ProjetoJava/src/projetojava/Jogo.java
@@ -1,15 +1,20 @@
 package projetojava;
 
+import java.util.ArrayList;
+
 
 public class Jogo {
+    
+    
+    private ArrayList<Jogo> ListaJogo = new ArrayList<>();
     private int id;
     private String titulo;
     private String descricao;
     private double preco;
     private String dataLancamento;
     private String desenvolvedora;
-    private String categoria;
-    private double tamanhoArquivo;
+    
+    
     
     
     
--- ProjetoJava/src/projetojava/ProjetoJava.java
+++ ProjetoJava/src/projetojava/ProjetoJava.java
@@ -1,10 +1,32 @@
 
 package projetojava;
+import java.util.Scanner;
 
 public class ProjetoJava {
 
     public static void main(String[] args) {
         
+        
+        
     }
+    
+    
+    public void menu(){
+        int op;
+        do{
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Bem Vindo!!/n");
+        System.out.println("1 - Cadastrar categoria");
+        System.out.println("2 - Cadastrar Jogos");
+        System.out.println("3 - Comprar Jogo");
+        System.out.println("4 - Relatorios");
+        System.out.println("5 - Sair");
+        op = entrada.nextInt();
+    }while(op != 5);
+    
+    
+    
+    }
+    
     
 }
