6601273dee2c9d7c08e0420751b7e766b24f568e
Higor <higorferreira240@gmail.com>
2025-09-23 23:26:39 -0300

    resolvendo bugs

--- ProjetoJava/src/projetojava/Jogo.java
+++ ProjetoJava/src/projetojava/Jogo.java
@@ -1,6 +1,7 @@
 package projetojava;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 
@@ -12,56 +13,9 @@ public class Jogo {
     private String titulo;
     private String descricao;
     private double preco;
-    private String dataLancamento;
+    private Date dataLancamento;
     private String desenvolvedora;
     private Categoria categoria;
-
-    public void cadastrarJogo(){
-        if (Categoria.categorias.isEmpty()) {
-            System.out.println("Nao existem categorias cadastradas!");
-            System.out.println("Cadastre pelo menos uma categoria antes de cadastrar um jogo.");
-            return;
-        }
-        Jogo jogo = new Jogo();
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Painel de Cadastro");
-        System.out.println("\nCategorias disponíveis:");
-        Categoria.listarCategorias();
-        
-        Categoria categoriaSelecionada = null;//alguns bugs para ser resolvido ainda nao ta 100%
-        while (categoriaSelecionada == null) {
-            System.out.println("Digite o codigo da categoria:");
-            if (entrada.hasNextInt()) {
-                int codigoCategoria = entrada.nextInt();
-                entrada.nextLine(); 
-                categoriaSelecionada = Categoria.buscarCategoriaPorCodigo(codigoCategoria);
-                if (categoriaSelecionada == null) {
-                    System.out.println("Codigo invalido! tente novamente.");
-                    System.out.println("Categorias disponíveis:");
-                    Categoria.listarCategorias();
-                }
-            } else {
-                System.out.println("digite um numero valido!");
-                entrada.nextLine(); 
-            }
-        }
-        System.out.println("Titulo do jogo:");
-        jogo.setTitulo(entrada.nextLine());
-        System.out.println("Descricao:");
-        jogo.setDescricao(entrada.nextLine());
-        System.out.println("Desenvolvedor:");
-        jogo.setDesenvolvedora(entrada.nextLine());
-        System.out.println("Data de lançamento:");
-        jogo.setDataLancamento(entrada.nextLine());
-        System.out.println("Preço:");
-        jogo.setPreco(entrada.nextDouble());
-        entrada.nextLine();
-        listaJogos.add(jogo);
-        System.out.println("Jogo Adicionado com Sucesso!!!");
-        //tem que fazer a logica para adicionar o id ainda
-        
-    }
-
 
     public int getId() {
         return id;
@@ -95,11 +49,11 @@ public class Jogo {
         this.preco = preco;
     }
 
-    public String getDataLancamento() {
+    public Date getDataLancamento() {
         return dataLancamento;
     }
 
-    public void setDataLancamento(String dataLancamento) {
+    public void setDataLancamento(Date dataLancamento) {
         this.dataLancamento = dataLancamento;
     }
 
@@ -110,6 +64,20 @@ public class Jogo {
     public void setDesenvolvedora(String desenvolvedora) {
         this.desenvolvedora = desenvolvedora;
     }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
     
     
+
+    public void cadastrarJogo(){
+        
+
+
+    }
 }
