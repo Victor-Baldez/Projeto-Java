8f5fb66ecf703811ea07818cf26b1e5f768a4c1b
KawaRodrigues17 <rodrigueskawa817@gmail.com>
2025-09-24 23:28:01 -0300

    Terminei validação de user e dev no menu - >>

--- ProjetoJava/src/projetojava/Steam.java
+++ ProjetoJava/src/projetojava/Steam.java
@@ -12,45 +12,56 @@ public class Steam {
     
     public static void menu(){
         int op;
-        String op2;
         
         Categoria categoria = new Categoria();
         do{
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Bem Vindo!!/n");
-        System.out.print("@ - Você é usuário ou densenvolvedor?");
+        
+        //fiz mais opções 
+        System.out.println("Bem Vindo!!\n");
+        System.out.print("- Voce e usuario ou densenvolvedor na Steam?\n");
+        System.out.println("- Digite (1) para Desenvolvedor:\n- Digite (2) para Usuario:");
+        System.out.println("- Sair (6): ");
         op = entrada.nextInt();
-        /*op2 = entrada.nextString();
         
-        if(op2 == true){
-           if(op2){
-              System.out.println("1 - Cadastrar categoria");
-              System.out.println("2 - Cadastrar Jogos");
-              System.out.println("4 - Sair");  
-           }
-           
-        }
-        */
-        System.out.println("3 - Comprar Jogos");
-        System.out.println("4 - Sair");
-        
-        
-        else if(op < 0 && op > 4){
+        if(op < 0 && op > 6){
             System.out.println("Opcao invalida!!");
             return;
         }else{
             
-            if (op == 1){
-                categoria = new Categoria();
-                categoria.cadastrarCategoria();
-                Categoria.categorias.add(categoria);
+            //para o dev
+            if(op == 1){
+                System.out.println("- Aqui estao as opcoes do desenvolvedor:");
+                System.out.println("3 - Cadastrar categoria: ");
+                System.out.println("4 - Cadastrar Jogos: ");
+                System.out.println("<------------->");
+                System.out.println("6 - Sair: ");
+                op = entrada.nextInt();
+                if (op == 3){
+                    categoria = new Categoria();
+                    categoria.cadastrarCategoria();
+                    Categoria.categorias.add(categoria);
+                }else if(op == 4){
+                    Jogo jogo = new Jogo();
+                    jogo.cadastrarJogo();
+               }
+                //para o user
+            }else if(op == 2){
+                System.out.println("- Aqui estao as opcoes de usuario:");
+                System.out.println("5 - Comprar Jogos: ");
+                System.out.println("<------------->");
+                System.out.println("6 - Sair: ");
+                op = entrada.nextInt();
+                if(op == 5){
+                   //comprar jogos função futura 
+                }  
+            }else{
+                System.out.println("Voce saiu da Steam.");
             }
-            else if(op == 2){
-                Jogo jogo = new Jogo();
-                jogo.cadastrarJogo();
-            }
+           
+            
         }
-    }while(op != 4);
+    }while(op != 6);
     
     
     }
@@ -60,6 +71,10 @@ public class Steam {
         //vc acha o nome
         //cadastra o jogo passando como parametro a categoria
     
+    // tem que arrumar isso pra poder selecionar categoria
+    //*acho que tem que colocar dentro do main*
+    // pra rodar certinho tem que comentar essa parte kkkkk!
+    /*
     public static String selecionarCat(){
         String catSel = "";
         System.out.println("Selecione o código da categoria");
@@ -78,5 +93,5 @@ public class Steam {
         
         
         
-    }
+    }*/
 }
