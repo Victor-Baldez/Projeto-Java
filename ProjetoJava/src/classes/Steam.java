dec2c8076f1ef662ccd34873fde8f72d421b463b
Higor <higorferreira240@gmail.com>
2025-10-10 09:37:51 -0300

    Algumas mudanças no login

--- ProjetoJava/src/classes/Steam.java
+++ ProjetoJava/src/classes/Steam.java
@@ -1,12 +1,12 @@
 
 package classes;
 import java.util.Scanner;
-
+import interfaces.TelaLogin;
 public class Steam {
 
     public static void main(String[] args) {
-       
-        
+       TelaLogin telaLogin = new TelaLogin();
+       telaLogin.setVisible(true);
     }
     
     
